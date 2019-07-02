2e2197922f88975c3e24160677735e185e76019d
xuin23 <xuin23@outlook.com>
2019-07-03 05:47:45 +0800

    vue

---
+++ h5/vue/app/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <el-row>
+      <el-col :span="6" :offset="8">
+        <el-input v-model="username" placeholder="邮箱"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6" :offset="8">
+        <el-input v-model="password" placeholder="密码" show-password></el-input>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
