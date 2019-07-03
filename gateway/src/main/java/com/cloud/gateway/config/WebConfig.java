3af60d2780920fa9862fb8145aa0f9557a361676
xuin23 <xuin23@outlook.com>
2019-07-03 12:42:34 +0800

    gataway 跨域请求

---
+++ gateway/src/main/java/com/cloud/gateway/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.cloud.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * 网络配置
+ */
+@Configuration
+public class WebConfig {
+
+    /**
+     * 配置跨域
+     *
+     * @return CorsWebFilter
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // cookie跨域
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        // 配置前端js允许访问的自定义响应头
+        config.addExposedHeader("setToken");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+}
