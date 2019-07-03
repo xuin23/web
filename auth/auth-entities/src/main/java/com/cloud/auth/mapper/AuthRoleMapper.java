2cfda65c46b20c3b9ddcd5a5b0db848109eceedb
xuin23 <xuin23@outlook.com>
2019-07-04 07:54:01 +0800

    authRole

--- auth/auth-entities/src/main/java/com/cloud/auth/mapper/AuthRoleMapper.java
+++ auth/auth-entities/src/main/java/com/cloud/auth/mapper/AuthRoleMapper.java
@@ -1,11 +1,11 @@
 package com.cloud.auth.mapper;
 
-import com.cloud.auth.entity.AuthUser;
+import com.cloud.auth.entity.AuthRole;
 import com.cloud.auth.mapper.base.BaseMapper;
 
 /**
  * 角色 mapper
  */
-public interface AuthRoleMapper extends BaseMapper<AuthUser> {
+public interface AuthRoleMapper extends BaseMapper<AuthRole> {
 
 }
