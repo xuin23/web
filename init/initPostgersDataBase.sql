db1e38bb5e78d8c738ac418580d3c8ade8ad4229
xuin23 <xuin23@outlook.com>
2019-12-03 22:57:19 +0800

    postgre database init

--- init/initPostgersDataBase.sql
+++ init/initPostgersDataBase.sql
@@ -22,9 +22,6 @@ comment on column web.AUTH_USER.STATUS is '状态';
 comment on column web.AUTH_USER.UPDATE_TIME is '更新日期';
 comment on column web.AUTH_USER.CREATE_TIME is '创建日期';
 
-insert into web.auth_user (username, password, realname, email, status)
-values ('xuin23', 'E10ADC3949BA59ABBE56E057F20F883E', '徐礼健', 'xxxx@xxx.com', 'TRUE');
-
 
 -- 角色表
 CREATE TABLE IF NOT EXISTS WEB.AUTH_ROLE
---
+++ init/initdata.sql
@@ -0,0 +1,2 @@
+insert into web.auth_user (username, password, realname, email, status)
+values ('xuin23', 'E10ADC3949BA59ABBE56E057F20F883E', '徐礼健', 'xxxx@xxx.com', 'TRUE');
