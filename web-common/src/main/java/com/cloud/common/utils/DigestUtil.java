ecea0267a589c165ec710fae1ddcf705e58197ed
xuin23 <xuin23@outlook.com>
2019-12-06 16:00:19 +0800

    加密工具

--- web-common/src/main/java/com/cloud/common/utils/DigestUtil.java
+++ web-common/src/main/java/com/cloud/common/utils/DigestUtil.java
@@ -9,7 +9,6 @@ import java.security.MessageDigest;
  */
 public class DigestUtil {
 
-    public static final String ENCODE = "UTF-8";
     private static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /**
