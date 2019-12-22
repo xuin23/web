556fc88b10f853c1405932ba2f8cb4d399dfede7
xuin23 <xuin23@outlook.com>
2019-12-23 01:42:26 +0800

    md5

--- web-common/src/main/java/com/cloud/common/utils/DigestUtil.java
+++ web-common/src/main/java/com/cloud/common/utils/DigestUtil.java
@@ -12,17 +12,26 @@ public class DigestUtil {
     private static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /**
-     * MD5 加密
+     * MD5 工具
      *
      * @param str 加密内容s
      * @return 加密结果
      */
     public static String encodeByMd5(String str) {
-        try {
-            byte[] strTemp = str.getBytes();
-            MessageDigest mdTemp = MessageDigest.getInstance(EncodeType.MD5.toString());
+        byte[] temp = str.getBytes();
+        return encodeByMd5(temp);
+    }
 
-            mdTemp.update(strTemp);
+    /**
+     * MD5 工具
+     *
+     * @param bytes byte数组
+     * @return 加密结果
+     */
+    public static String encodeByMd5(byte[] bytes) {
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance(EncodeType.MD5.toString());
+            mdTemp.update(bytes);
             byte[] md = mdTemp.digest();
             int j = md.length;
             char[] string = new char[j * 2];
