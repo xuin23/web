c441b9cb39da1320d6c390bdcf79b17777f24189
xuin23 <xuin23@outlook.com>
2019-12-24 22:15:21 +0800

    RedisConfiguration.java

--- web-message/message-core/src/main/java/com/cloud/message/config/RedisConfiguration.java
+++ web-message/message-core/src/main/java/com/cloud/message/config/RedisConfiguration.java
@@ -31,7 +31,6 @@ public class RedisConfiguration {
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
