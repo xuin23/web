056bc5d4a6cb8a217b91fa254abd83c0d53175ba
xuin23 <xuin23@outlook.com>
2019-12-26 23:27:24 +0800

    securityTest

---
+++ web-test/test-security/src/main/java/com/cloud/test/security/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.cloud.test.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class TestController {
+
+    @GetMapping("/")
+    public String TestRequest() {
+        return "security";
+    }
+}
