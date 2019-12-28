1ed092f1bc8170dd3f5616d6b6cba161d4fc467e
xuin23 <xuin23@outlook.com>
2019-12-29 01:06:22 +0800

    Status

--- web-common/src/main/java/com/cloud/common/enums/Status.java
+++ web-common/src/main/java/com/cloud/common/enums/Status.java
@@ -5,17 +5,6 @@ package com.cloud.common.enums;
  * 状态
  */
 public enum Status {
-
-    TRUE("有效"),
-    FALSE("无效");
-
-    private String value;
-
-    Status(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    TRUE,
+    FALSE
 }
