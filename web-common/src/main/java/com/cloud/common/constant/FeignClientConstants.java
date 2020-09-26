ef72384dc7f1ca0c600d16483dc3552d53e3d572
xulijian <xuin23@outlook.com>
2020-09-27 01:50:09 +0800

    服务名称修改

--- web-common/src/main/java/com/cloud/common/constant/FeignClientConstants.java
+++ web-common/src/main/java/com/cloud/common/constant/FeignClientConstants.java
@@ -8,11 +8,11 @@ package com.cloud.common.constant;
 public class FeignClientConstants {
 
     //权限
-    public static final String AUTH_APPLICATION_NAME_CODE = "/auth-core/cloud";
-    public static final String AUTH_EUREKA_SERVER_INSTANCE_CORE = "AUTH-CORE";
+    public static final String AUTH_APPLICATION_NAME_CODE = "/auth/cloud";
+    public static final String AUTH_EUREKA_SERVER_INSTANCE_CORE = "AUTH";
 
     //消息
-    public static final String MESSAGE_APPLICATION_NAME_CODE = "/message-core/cloud";
-    public static final String MESSAGE_EUREKA_SERVER_INSTANCE_CORE = "MESSAGE-CORE";
+    public static final String MESSAGE_APPLICATION_NAME_CODE = "/message/cloud";
+    public static final String MESSAGE_EUREKA_SERVER_INSTANCE_CORE = "MESSAGE";
 
 }
