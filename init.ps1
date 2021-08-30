a8f55fa9ac6093926d77f8413b55517c81f7a872
徐礼健 <xuin23@outlook.com>
2021-08-30 16:46:33 +0800

    脚本修改

--- init.ps1
+++ init.ps1
@@ -2,25 +2,23 @@
 $path = Get-Location 
 
 # 启动并初始化基本服务 例如redis postgres
-Set-Location init/shell/
+Set-Location $path/init
 ./baseService.ps1
 
 # 代码初始化
 Set-Location $path
+
 mvn clean
 mvn install
 
 # 创建并启动gateway容器
-Set-Location $path
-Set-Location web-service/service-gateway/
+Set-Location $path/web-service/service-gateway/
 ./build.ps1
 
 # 创建并启动auth-core容器
-Set-Location $path
-Set-Location web-service/service-auth-core/
+Set-Location $path/web-service/service-auth/
 ./build.ps1
 
-# 创建并启动message-core容器
-Set-Location $path
-Set-Location web-message/service-message-core/
+# 创建并启动message容器
+Set-Location $path/web-service/service-message/
 ./build.ps1
